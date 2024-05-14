a2c3a0e5d1c1dbd81da61b61a37dc93bc91fdb24
{yasaiseikatu} <{kd1314617@st.kobedenshi.ac.jp}>
2024-05-14 11:55:37 +0900

    プルダウン

---
+++ public/pulldown.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pulldown.php</title>
+</head>
+
+<body>
+    <?php
+    echo  '<p>あなたの好きなフルーツは、'  . $_POST['fruit']  . 'ですね。</p>';
+    ?>
+    <a href='pulldown.html'>戻る</a>
+</body>
+
+</html>

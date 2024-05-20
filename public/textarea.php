bd470f2bdb37594dd032ad000e0a0ea780d00091
{yasaiseikatu} <{kd1314617@st.kobedenshi.ac.jp}>
2024-05-20 13:15:05 +0900

    text

---
+++ public/textarea.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>textarea.php</title>
+</head>
+
+<body>
+  <h4></h4>
+  <p>入力された文章は、つぎのとおりです。</p>
+  <?php
+  echo '<p>' . $_POST['data'] . '</p>';
+  ?>
+  <a href='textarea.html'>戻る</a>
+</body>
+
+</html>
